7b47a795b37e3736ceaf6c08d96627dd9bfffd68
Balachandar1997 <117364951+Balachandar1997@users.noreply.github.com>
2022-12-13 13:47:44 +0530

    commit

---
+++ .github/product/src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+import Demoform from './component/Demoform';
+//import Parent from './component/Parent';
+//import Ternary from './component/ternary';
+//import Listpost from './component/Listpost';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <Parent /> */}
+       {/* <Listpost /> */}
+       <Demoform />
+
+       
+    </div>
+  );
+}
+
+export default App;
